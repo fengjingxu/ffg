6c8dacc87ecfd1f0cf274bb4925c591a228d44b3
冯晶旭 <apple@appledemacbook-pro.local>
2018-09-13 22:00:50 +0800

    Initial Commit

---
+++ 对方的身份/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  对方的身份
+//
+//  Created by apple on 2018/9/13.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
